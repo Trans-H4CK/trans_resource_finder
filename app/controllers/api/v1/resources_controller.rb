bfe7da5e10c7bece21ece197e52ecd69d6979749
Hannah Howard <hannah@hannahhoward.net>
2013-09-14 15:48:49 -0700

    Inital spec

--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::ResourcesController < ApplicationController
   def show
     respond_with Resource.find(params[:id])
   end
+
+  def index
+    @category_string = params[:category]
+
+    if @category_string
+      @category = Category.where(:name => @category_string).first
+      @resources = Resource.where(:category_id => @category.id)
+    else
+      @resources = Resource.all
+    end
+
+    respond_with @resources
+  end
+
+  def geocode(zip_code)
+    Rails.application.config.geocoder.geocode(:zipcode => zip_code)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ TransResourceFinder::Application.routes.draw do
 
   namespace :api, :defaults => {:format => 'json'} do
     namespace :v1, :constraints => ApiConstraints.new(:version => 1) do
-      resources :resources, :only => [:show]
+      resources :resources, :only => [:show, :index]
     end
   end
 end
