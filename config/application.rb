e2976e93f1eec6bb77033485b09dd0fe970b30d1
Hannah Howard <hannah@hannahhoward.net>
2013-09-15 11:55:25 -0700

    Revert precompile

--- config/application.rb
+++ config/application.rb
@@ -105,6 +105,5 @@ module TransResourceFinder
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
   end
 end
