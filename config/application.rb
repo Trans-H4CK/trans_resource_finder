8e6c91a64be9f548f11ce1ac374369064bcaa79b
Hannah Howard <hannah@hannahhoward.net>
2013-09-15 11:41:37 -0700

    Fix heroku?

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,6 @@
+
 require File.expand_path('../boot', __FILE__)
+
 
 # Pick the frameworks you want:
 require "active_record/railtie"
@@ -6,17 +8,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-require 'active_record/connection_adapters/postgis_adapter/railtie'
-require 'rgeo/active_record'
-
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
-
 class ActiveRecordOverrideRailtie < Rails::Railtie
   initializer "active_record.initialize_database.override" do |app|
 
@@ -37,6 +28,17 @@ class ActiveRecordOverrideRailtie < Rails::Railtie
       end
     end
   end
+end
+
+# require "rails/test_unit/railtie"
+require 'active_record/connection_adapters/postgis_adapter/railtie'
+require 'rgeo/active_record'
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module TransResourceFinder
