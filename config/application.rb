e9f48891853e9b51c9514673fcc112a2d501de1e
Hannah Howard <hannah@hannahhoward.net>
2013-09-15 11:53:43 -0700

    Fix precompilation

--- config/application.rb
+++ config/application.rb
@@ -104,5 +104,7 @@ module TransResourceFinder
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
